5de48eeeed9f350c65a7075dab0973206315ed92
aneesh3 <45024754+aneesh3@users.noreply.github.com>
2018-11-14 17:54:12 -0800

    Update part1.sh

--- part1.sh
+++ part1.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-echo Script has been loaded.
+echo Script has been loaded, running updates.
 sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install dnsmasq hostapd
+echo Running upgrades.
+sudo apt-get upgrade -y
+echo Downloading files, afterwards stopping services to reboot.
+sudo apt-get install dnsmasq hostapd -y
 sudo systemctl stop dnsmasq
 sudo systemctl stop hostapd
 sudo reboot
